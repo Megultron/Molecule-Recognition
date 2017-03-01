8f830603d3aa0f4a6807277c5d7a55e828878f81
Jason Trader <jasonmtrader@gmail.com>
2017-02-28 17:46:18 -0800

    convert to grayscale

--- sobel.py
+++ sobel.py
@@ -1,5 +1,10 @@
 from PIL import Image, ImageFilter
 
 mol = Image.open("mol.png")
-mol = mol.filter(ImageFilter.FIND_EDGES)
+#convert to grayscale (8-bit)
+mol = mol.convert('L')
+#experimentally defined magic number
+threshold = 90
+mol = mol.point((lambda x: 0 if x<threshold else 255), '1')
+mol.save("modified.png")
 mol.show()
