8ae37b90ac862ef11d7d8f1cd44009bf60a0173e
Jason Trader <jasonmtrader@gmail.com>
2017-04-05 07:52:18 -0700

    Added starting script for file transfer

---
+++ script.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+d = webdriver.Firefox()
+final = []
+
+start = 608749
+while start < 608771:
+    print (start)
+    d.get("https://www.thoughtco.com/a-organic-compounds-list-" + str(start))
+    try:
+        f= d.find_element_by_id("flex_1-0")
+        lines = f.text.splitlines()
+        for x in lines:
+            if '-' in x:
+                final.append(x.split(' - '))
+    except NoSuchElementException:
+        #Ignore this case
+        print 'goteeeeem'
+    #print(f.text.splitlines())
+
+    start += 1
+
+print final
