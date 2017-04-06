153c624a9abefa6804558248fabdfd2ebb7e880a
Jason Trader <jasonmtrader@gmail.com>
2017-04-06 11:35:24 -0700

    For retrieval on another device.

--- script.py
+++ script.py
@@ -1,25 +1,36 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 d = webdriver.Firefox()
 final = []
 
 start = 608749
-while start < 608771:
-    print (start)
-    d.get("https://www.thoughtco.com/a-organic-compounds-list-" + str(start))
+i = 0
+delay = 3
+while i< 26:
+    print chr(ord('a')+i)
+    d.get("https://www.thoughtco.com/a-organic-compounds-list-" + str(start+i))
     try:
+        WebDriverWait(d, delay).until(EC.presence_of_element_located(d.find_element_by_id("flex_1-0")))
         f= d.find_element_by_id("flex_1-0")
         lines = f.text.splitlines()
         for x in lines:
             if '-' in x:
                 final.append(x.split(' - '))
-    except NoSuchElementException:
+    except TimeoutException:
         #Ignore this case
         print 'Page not present'
     #print(f.text.splitlines())
 
-    start += 1
-
-print final
+    i += 1
+"""
 d.get("https://pubchem.ncbi.nlm.nih.gov/")
-d.quit()
+d.find_element_by_id("term").send_keys(final[0][0].strip())
+d.find_element_by_id("go").click()
+time.sleep(1)
+print d.find_elements_by_class_name("rsltimg")
+"""
